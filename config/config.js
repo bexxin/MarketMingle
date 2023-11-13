299ed81678707d4861893cba672f305788de956f
bexxin <109035845+bexxin@users.noreply.github.com>
2023-11-13 13:19:34 -0500

    updated server.js and config.js

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const config = {env: process.env.NODE_ENV || 'development', 
+ port: process.env.PORT || 3000,
+ jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
+ mongoUri: process.env.MONGODB_URI || "mongodb+srv://marketmingle:6mODVrpJ96kUET5w@cluster0.dlwzq1d.mongodb.net/?retryWrites=true&w=majority"||
+process.env.MONGO_HOST ||
+ 'mongodb://' + (process.env.IP || 'localhost') + ':' + 
+(process.env.MONGO_PORT || '27017') +
+ '/mernproject' 
+ }
+ export default config
