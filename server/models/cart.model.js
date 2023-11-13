391a6912342b0a654282d8bf63bcdd4b7e0a65ac
bexxin <109035845+bexxin@users.noreply.github.com>
2023-11-13 15:33:09 -0500

    updated cartschema to include product/quantity

--- server/models/cart.model.js
+++ server/models/cart.model.js
@@ -1,9 +1,13 @@
 import mongoose from 'mongoose'
 
+const CartItemSchema = new mongoose.Schema({
+    product:{type:mongoose.Schema.Types.ObjectId, ref:"Product", required:true},
+    quantity: {type:Number,required:true}
+});
 
 const CartSchema = new mongoose.Schema({
     user:{type:mongoose.Schema.Types.ObjectId, ref:'User',required:true},
-    items:[{type:mongoose.Schema.Types.ObjectId, ref:'Product', required:true}],
+    items:[CartItemSchema],
     subtotal:{type:Number, required: true},
     total:{type:Number, required: true}
 });
