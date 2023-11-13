60441273988fb7c9eab3eeaffdf1a2c1e524f621
bexxin <109035845+bexxin@users.noreply.github.com>
2023-11-13 15:23:15 -0500

    minor error fixed

--- server/models/order.model.js
+++ server/models/order.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
  const OrderSchema = new mongoose.Schema({
     price:{type:Number, required:true},
-    products:[{type:mongoose.Schema.Types.ObjectId, ref:'Cart',required:true}],
+    products:[{type:mongoose.Schema.Types.ObjectId, ref:'Product',required:true}],
     user:{type:mongoose.Schema.Types.ObjectId, ref:'User',required:true},
     date:{type:Date, default: Date.now},
     status:{type:String, default:"Pending"}
