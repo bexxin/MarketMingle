8b98de389996449e3014b2a513865337c2ef1107
bexxin <109035845+bexxin@users.noreply.github.com>
2023-11-14 12:28:55 -0500

    merged server.js

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
-const express=require('express');
-const app = express();
-app.use('/',function(req, res){
-    res.send('Hello, Welcome to MarketMingle');
+//Entrypoint and primary config for server
+import config from './config/config.js' 
+import app from './server/express.js'
+import mongoose from 'mongoose' 
+
+
+mongoose.Promise = global.Promise
+
+//Connect to database
+mongoose.connect(config.mongoUri, { useNewUrlParser: true,
+//useCreateIndex: true, 
+useUnifiedTopology: true } )
+
+ .then(() => {
+console.log("Connected to the Marketmingle database!");
+})
+
+mongoose.connection.on('error', () => {
+throw new Error(`unable to connect to database: ${config.mongoUri}`) 
+})
+
+
+app.get("/", (req, res) => {
+res.json({ message: "Welcome to MarketMingle!" });
+
 });
-app.listen(3000);
-console.log('Server is running at http://localhost:3000/');
-module.exports=app;
+app.listen(config.port, (err) => { 
+if (err) {
+console.log(err) 
+}
+console.info('Server started on port %s.', config.port) 
+console.info('URI %s.', config.mongoUri) 
+})
