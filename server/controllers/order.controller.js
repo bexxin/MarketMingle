daa12d57ca25a3315c437755b4b6d39dbba4558d
bexxin <109035845+bexxin@users.noreply.github.com>
2023-11-23 17:38:37 -0500

    blank order functions to be completed

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -1,7 +1,7 @@
 import Order from '../models/order.model.js';
 import Cart from '../models/cart.model.js';
 import errorHandler from './error.controller.js'
-
+//Create order
 const createOrder=async(req,res)=>{
     try{
     //get cart
@@ -25,12 +25,24 @@ const createOrder=async(req,res)=>{
         return res.status(400).json({error:errorHandler.getErrorMessage(err)});
     }
 }
+//Retrieve orders
+const getOrders=async(req,res)=>{
+    //logic to retrieve all order
+}
+const getOrdersById=async(req,res)=>{
+    //logic to get order by order id
+}
+const getOrdersByUser=async(req,res)=>{
+    //logic to get order by user
+}
 
+//Update Order
 const updateOrder=async(req,res)=>{
     //logic for admin to update order 
 }
+//Delete order
 const deleteOrder=async(req,res)=>{
     //logic for admin to delete order 
 }
 
-export default{createOrder}
+export default{createOrder,getOrders,getOrdersById,getOrdersByUser,updateOrder,deleteOrder}
