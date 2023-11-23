71f73c43574407323e8f45206259f8122668fbf6
bexxin <109035845+bexxin@users.noreply.github.com>
2023-11-23 18:06:22 -0500

    more functions added to order controller

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -27,13 +27,29 @@ const createOrder=async(req,res)=>{
 }
 //Retrieve orders
 const getOrders=async(req,res)=>{
-    //logic to retrieve all order
+    try{
+        const orders = await Order.find();
+        return res.status(200).json(orders);
+    }catch(err){
+        return res.status('400').json({error: errorHandler.getErrorMessage.apply(err) + "Could not retrieve orders"});
+    }
 }
 const getOrdersById=async(req,res)=>{
-    //logic to get order by order id
+    try{
+        const order = await Order.findById(req.params.id)
+        return res.status(200).json(order);
+
+    }catch(err){
+        return res.status('400').json({error: errorHandler.getErrorMessage(err) + "Could not retrieve order"});
+    }
 }
 const getOrdersByUser=async(req,res)=>{
-    //logic to get order by user
+    try{
+        const orders = await Order.find({user:req.params.userId});
+        return res.status(200).json(orders);
+    }catch(err){
+        return res.status(400).json({error: errorHandler.getErrorMessage(err) + "Could not retrieve orders."});
+    }
 }
 
 //Update Order
