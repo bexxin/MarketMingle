725fcc9962f20379a10dfd5893ca9683659a1989
bexxin <109035845+bexxin@users.noreply.github.com>
2023-11-23 16:49:01 -0500

    addToCart function added to controller

--- server/controllers/cart.controller.js
+++ server/controllers/cart.controller.js
@@ -1,5 +1,49 @@
 import Cart from '../models/cart.model.js';
+import CartItem from '../models/cart.model.js';
+import Product from '../models/product.model.js';
 
-const addToCart=asyn(req,res) => {
+const addToCart=async(req,res) => {
+   try{
+    const taxRate=1.13;
+    //get product ID and quantity 
+    const {productId, quantity} = req.body
+    //Get product based on id
+    const product = await Product.findById(productId);
+    if(!product){
+        return res.status(404).json({error:'Product not found.'});
+    }
+    //create new cart if user doesnt already have one
+    let cart = await Cart.findOne({user:req.auth._id});
+    if(!cart){
+        cart = new Cart({user:req.auth._id, items:[],subtotal:0,total:0});
+    }
+    //update quantity if product already in cart
+    const existingItem = cart.items.find(item=>item.product.equals(productId));
+    if(existingItem){
+        existingItem.quantity += quantity;
+    }else{
+        //create new cart item if not already in cart
+        const newCartItem=new CartItem({product:productId,quantity});
+        //add to items array
+        cart.items.push(newCartItem);
+    }
+    //update totals
+    cart.subtotal += product.price*quantity;
+    cart.total=cart.subtotal * taxRate;
+    //save cart to database
+    await cart.save();
+    return res.status(200).json({message:"Product successfully added to cart."})
+   }catch(err){
+    return res.status(400).json({
+        error: errorHandler.getErrorMessage(err)
+    })
+   }
+} 
 
-}
+export default {addToCart};
+
+
+
+
+
+
