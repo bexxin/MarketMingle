945c362583da789b23f5055d45cc7c985323f61a
bexxin <109035845+bexxin@users.noreply.github.com>
2023-11-23 17:16:24 -0500

    order model fixed

--- server/models/order.model.js
+++ server/models/order.model.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
- const OrderSchema = new mongoose.Schema({
-    price:{type:Number, required:true},
-    products:[{type:mongoose.Schema.Types.ObjectId, ref:'Product',required:true}],
-    user:{type:mongoose.Schema.Types.ObjectId, ref:'User',required:true},
-    date:{type:Date, default: Date.now},
-    status:{type:String, default:"Pending"}
-
-});
-const Order = mongoose.model("Order",OrderSchema);
+const orderSchema = new mongoose.Schema({
+    user:{type:mongoose.Schema.Types.ObjectId,ref:'User', required: true},
+    cart:{type:mongoose.Schema.Types.ObjectId,ref:'Cart',required:true},
+    shippingAddress: {
+        street: {type: String, required: 'Street is required'},
+        city: {type: String, required: 'City is required'},
+        Province: {type: String,required:'Province is required'},
+        PostalCode: {type: String, required: 'Postal Code is required'}
+      },
+    created:{type:Date,default:Date.now},
+    status:{type:String,default:'Not processed', enum:['Not processed', 'Processing','Shipped','Delivered']}
+})
+const Order = mongoose.model('Order',orderSchema)
 export default Order;
