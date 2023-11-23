8f727b735950b422032de7419ff961ac3b54fc38
bexxin <109035845+bexxin@users.noreply.github.com>
2023-11-23 16:01:57 -0500

    updated product model

---
+++ server/controllers/cart.controller.js
@@ -0,0 +1,5 @@
+import Cart from '../models/cart.model.js';
+
+const addToCart=asyn(req,res) => {
+
+}
--- server/models/product.model.js
+++ server/models/product.model.js
@@ -3,10 +3,12 @@ import mongoose from 'mongoose'
 
 const ProductSchema = new mongoose.Schema({
     name:{type:String, required:true},
+    image:{data:Buffer,contentType:String},
     description:{type:String, required:true},
     price:{type:Number, required:true},
     quantity:{type:Number, required:true},
-    category:{type:String, required:true}
+    category:{type:String, required:true},
+    created:{type:Date,default:Date.now}
 });
 const Product = mongoose.model("Product",ProductSchema);
 export default Product;
