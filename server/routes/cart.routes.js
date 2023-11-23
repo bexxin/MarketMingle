c43cfd7cb83550fc79e201658f4ad5a1e61e873d
bexxin <109035845+bexxin@users.noreply.github.com>
2023-11-23 17:54:21 -0500

    cart routes

--- server/routes/cart.routes.js
+++ server/routes/cart.routes.js
@@ -1,2 +1,11 @@
 import express from 'express'
 import cartController from '../controllers/cart.controller'
+
+const router=express.Router();
+
+//CRUD operations
+router.post('/api/carts/add',cartController.addToCart);
+router.delete('/api/carts/remove/:productId',cartController.removeFromCart)
+
+
+export default router;
