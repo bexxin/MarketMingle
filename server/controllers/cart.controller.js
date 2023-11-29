5e46c84e2bb915458f3cdce6f4a9b2e0c2c72a11
bexxin <109035845+bexxin@users.noreply.github.com>
2023-11-29 18:19:12 -0500

    addToCart working!

--- server/controllers/cart.controller.js
+++ server/controllers/cart.controller.js
@@ -1,72 +1,72 @@
 import Cart from '../models/cart.model.js';
+import CartItem from '../models/cartItem.model.js';
 import Product from '../models/product.model.js';
-import errorHandler from './error.controller.js'
-//Create/update cart-logic combined, cart is created if user adds item and no cart exists
-const addToCart=async(req,res) => {
-    console.log("addToCart called");
-   try{
-    const taxRate=1.13;
-    //get product ID and quantity 
-    const {productId, quantity} = req.body
-    //Get product based on id
-    const product = await Product.findById(productId);
-    if(!product){
-        return res.status(404).json({error:'Product not found.'});
+import errorHandler from './error.controller.js';
+
+const findOrCreateCart = async (userId) => {
+    let cart = await Cart.findOne({ user: userId });
+    if (!cart) {
+        cart = new Cart({ user: userId, items: [], subtotal: 0, total: 0 });
+        await cart.save();
     }
-    //create new cart if user doesnt already have one
-     let cart = await Cart.findOne({user:req.auth._id});
-    if(!cart){
-        cart = new Cart({user:req.auth._id, items:[],subtotal:0,total:0});
+    return cart;
+};
+
+const addToCart = async (req, res) => {
+    try {
+        const taxRate = 1.13;
+        const { productId, quantity } = req.body;
+
+        const product = await Product.findById(productId);
+        if (!product) {
+            return res.status(404).json({ error: 'Product not found.' });
+        }
+
+        const userCart = await findOrCreateCart(req.auth._id);
+
+        const existingItem = userCart.items.find(item => item.product.equals(productId));
+
+        if (existingItem) {
+            existingItem.quantity += quantity;
+        } else {
+            const newCartItem = new CartItem({ product: productId, quantity });
+            userCart.items.push(newCartItem);
+        }
+
+        userCart.subtotal += product.price * quantity;
+        userCart.total = userCart.subtotal * taxRate;
+
+        await userCart.save();
+
+        return res.status(200).json({ message: 'Product successfully added to cart.', cart: userCart });
+    } catch (err) {
+        return res.status(400).json({ error: errorHandler.getErrorMessage(err) });
     }
-    //update quantity if product already in cart
-    const existingItem = cart.items.find(item=>item.product.equals(productId));
-    if(existingItem){
-        existingItem.quantity += quantity;
-    }else{
-        //create new cart item if not already in cart
-        const newCartItem=new CartItem({product:productId,quantity});
-        //add to items array
-        cart.items.push(newCartItem);
+};
+
+const removeFromCart = async (req, res) => {
+    try {
+        const { productId } = req.params;
+        const userCart = await findOrCreateCart(req.auth._id);
+
+        const itemIndex = userCart.items.findIndex(item => item.product.equals(productId));
+
+        if (itemIndex !== -1) {
+            userCart.items.splice(itemIndex, 1);
+            await userCart.save();
+
+            if (userCart.items.length === 0) {
+                await Cart.deleteOne({ user: req.auth._id });
+                return res.status(200).json({ message: 'Your cart is now empty.' });
+            }
+
+            return res.status(200).json({ message: 'Item successfully removed from cart.' });
+        }
+
+        return res.status(404).json({ message: 'Item not found in the cart.' });
+    } catch (err) {
+        return res.status(400).json({ error: errorHandler.getErrorMessage(err) });
     }
-    //update totals
-    cart.subtotal += product.price*quantity;
-    cart.total=cart.subtotal * taxRate;
-    //save cart to database
-    await cart.save();
-    return res.status(200).json({message:"Product successfully added to cart."})
-   }catch(err){
-    return res.status(400).json({error: errorHandler.getErrorMessage(err)});
-   }
-} 
-//Update/delete Cart- logic combined, cart is deleted when empty
-const removeFromCart=async(req,res)=>{
-    try{
-    const{productId}=req.params;
-    //Find user's cart
-    const cart = await Cart.findOne({user:req.auth._id});
-    //get index of item in the items array
-    const itemIndex = cart.items.findIndex(item => item.product.equals(productId));
-    //remove product from items array
-    cart.items.splice(itemIndex,1)
-    //save updated cart
-    await cart.save();
-    //Delete cart if it is empty
-    if(cart.items.length ===0){
-        await Cart.deleteOne({user:req.auth._id});
-        return res.status(200).json({message:"Your cart is now empty."});
-    } 
-    
-    return res.status(200).json({message:"Item successfully removed from cart."});
-       
+};
 
-    }catch(err){
-        return res.status(400).json({error:errorHandler.getErrorMessage(err)});
-    }
-}
-export default {addToCart,removeFromCart};
-
-
-
-
-
-
+export default { addToCart, removeFromCart };
--- server/models/cart.model.js
+++ server/models/cart.model.js
@@ -1,17 +1,11 @@
 import mongoose from 'mongoose'
 
-const CartItemSchema = new mongoose.Schema({
-    product:{type:mongoose.Schema.Types.ObjectId, ref:"Product", required:true},
-    quantity: {type:Number,required:true}
-});
-
-
 const CartSchema = new mongoose.Schema({
-    user:{type:mongoose.Schema.Types.ObjectId, ref:'User',required:true},
-    items:[CartItemSchema],
+    user:{type:mongoose.Schema.Types.ObjectId, ref:'User',required:true, unique: true},
+    items:[{type:mongoose.Schema.Types.ObjectId, ref:'CartItem'}],
     subtotal:{type:Number, required: true},
     total:{type:Number, required: true}
 });
-const Cart = mongoose.model("Cart",CartSchema);
+const Cart = mongoose.model("Cart", CartSchema);
 
-export default {Cart};
+export default Cart;
