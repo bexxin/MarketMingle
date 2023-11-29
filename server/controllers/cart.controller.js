a318d2a1d570787112ab792e106537243de6de3f
bexxin <109035845+bexxin@users.noreply.github.com>
2023-11-29 18:32:04 -0500

    updated comments for cart controller

--- server/controllers/cart.controller.js
+++ server/controllers/cart.controller.js
@@ -2,7 +2,7 @@ import Cart from '../models/cart.model.js';
 import CartItem from '../models/cartItem.model.js';
 import Product from '../models/product.model.js';
 import errorHandler from './error.controller.js';
-
+//find existing cart or create new cart
 const findOrCreateCart = async (userId) => {
     let cart = await Cart.findOne({ user: userId });
     if (!cart) {
@@ -21,21 +21,22 @@ const addToCart = async (req, res) => {
         if (!product) {
             return res.status(404).json({ error: 'Product not found.' });
         }
-
+        //find/create user cart
         const userCart = await findOrCreateCart(req.auth._id);
-
+        //update quantity if product already in cart
         const existingItem = userCart.items.find(item => item.product.equals(productId));
 
         if (existingItem) {
             existingItem.quantity += quantity;
         } else {
+            //create new cart item if not already in cart
             const newCartItem = new CartItem({ product: productId, quantity });
             userCart.items.push(newCartItem);
         }
-
+        //update cart totals
         userCart.subtotal += product.price * quantity;
         userCart.total = userCart.subtotal * taxRate;
-
+        //save to database
         await userCart.save();
 
         return res.status(200).json({ message: 'Product successfully added to cart.', cart: userCart });
@@ -47,19 +48,20 @@ const addToCart = async (req, res) => {
 const removeFromCart = async (req, res) => {
     try {
         const { productId } = req.params;
+        //find user's cart
         const userCart = await findOrCreateCart(req.auth._id);
-
+        //get index of item in items array
         const itemIndex = userCart.items.findIndex(item => item.product.equals(productId));
-
+        //check if item is in array
         if (itemIndex !== -1) {
             userCart.items.splice(itemIndex, 1);
             await userCart.save();
-
+            //return message if cart is empty
             if (userCart.items.length === 0) {
                 await Cart.deleteOne({ user: req.auth._id });
                 return res.status(200).json({ message: 'Your cart is now empty.' });
             }
-
+            //return success message if items still in cart after deletion
             return res.status(200).json({ message: 'Item successfully removed from cart.' });
         }
 
