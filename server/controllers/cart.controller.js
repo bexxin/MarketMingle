c3346243c4a0e2bb925aa9a51976ed56c49d3bd2
bexxin <109035845+bexxin@users.noreply.github.com>
2023-11-30 09:35:25 -0500

    removeFromCart not working yet

--- server/controllers/cart.controller.js
+++ server/controllers/cart.controller.js
@@ -4,7 +4,8 @@ import Product from '../models/product.model.js';
 import errorHandler from './error.controller.js';
 //find existing cart or create new cart
 const findOrCreateCart = async (userId) => {
-    let cart = await Cart.findOne({ user: userId });
+    //populate cartitem with product document
+    let cart = await Cart.findOne({ user: userId }).populate('items.product');
     if (!cart) {
         cart = new Cart({ user: userId, items: [], subtotal: 0, total: 0 });
         await cart.save();
@@ -24,15 +25,13 @@ const addToCart = async (req, res) => {
         //find/create user cart
         const userCart = await findOrCreateCart(req.auth._id);
         //update quantity if product already in cart
-        const existingItem = userCart.items.find(item => item.product.equals(productId));
-
-        if (existingItem) {
-            existingItem.quantity += quantity;
-        } else {
-            //create new cart item if not already in cart
-            const newCartItem = new CartItem({ product: productId, quantity });
-            userCart.items.push(newCartItem);
-        }
+        console.log("userCart:",userCart);
+        console.log("userCart.items:", userCart.items);
+        
+        //create new cart item 
+        const newCartItem = new CartItem({ product: productId, quantity });
+        userCart.items.push(newCartItem);
+        
         //update cart totals
         userCart.subtotal += product.price * quantity;
         userCart.total = userCart.subtotal * taxRate;
@@ -50,22 +49,22 @@ const removeFromCart = async (req, res) => {
         const { productId } = req.params;
         //find user's cart
         const userCart = await findOrCreateCart(req.auth._id);
-        //get index of item in items array
-        const itemIndex = userCart.items.findIndex(item => item.product.equals(productId));
-        //check if item is in array
-        if (itemIndex !== -1) {
-            userCart.items.splice(itemIndex, 1);
-            await userCart.save();
-            //return message if cart is empty
-            if (userCart.items.length === 0) {
-                await Cart.deleteOne({ user: req.auth._id });
-                return res.status(200).json({ message: 'Your cart is now empty.' });
+        
+        for(let i = 0; i < userCart.items.length; i++){
+            if (userCart.items[i].ObjectId.equals(productId)){
+                userCart.items.splice(i,1)
+                break;
             }
-            //return success message if items still in cart after deletion
-            return res.status(200).json({ message: 'Item successfully removed from cart.' });
         }
-
-        return res.status(404).json({ message: 'Item not found in the cart.' });
+        await userCart.save();
+         //return message if cart is empty
+        if (userCart.items.length === 0) {
+             await Cart.deleteOne({ user: req.auth._id });
+             return res.status(200).json({ message: 'Your cart is now empty.' });
+         }
+        //return success message if items still in cart after deletion
+        return res.status(200).json({ message: 'Item successfully removed from cart.' });
+        
     } catch (err) {
         return res.status(400).json({ error: errorHandler.getErrorMessage(err) });
     }
