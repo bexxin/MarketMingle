4001d11524dd4b1b68ea2221d1795051485aa0eb
bexxin <109035845+bexxin@users.noreply.github.com>
2023-12-01 21:55:34 -0500

    functioning textfield for quantity

--- client/core/Products.jsx
+++ client/core/Products.jsx
@@ -7,6 +7,7 @@ import Typography from '@material-ui/core/Typography';
 import BaseballImg from './../assets/images/baseball.jpg';
 import Button from '@material-ui/core/Button'
 import { CardActions } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
 
 const Products = () => {
     const [products, setProducts] = useState([]);
@@ -15,6 +16,10 @@ const Products = () => {
 //TODO Add logic to get product data from database
 
 //add function to add to cart
+//function when quantity field is changed
+const handleQuantityChange = (event, setQuantity) => {
+    setQuantity(event.target.value);
+}
 
 const useStyles = makeStyles(theme => ({
     card: {
@@ -32,11 +37,14 @@ const useStyles = makeStyles(theme => ({
     },
     cardActions:{
         display:'flex',
+        flexDirection:"column",
+
         justifyContent: 'center'
     }
   }));
   
   export default function Home(){ 
+  const[quantity, setQuantity]=React.useState(1);
   const classes = useStyles()
   return (
   <Card className={classes.card}>
@@ -53,6 +61,7 @@ const useStyles = makeStyles(theme => ({
   <Typography component="h5">Price: 30.99</Typography>
   </CardContent>
   <CardActions className={classes.cardActions}>
+    <TextField type="number" label="Quantity" value={quantity} className={classes.quantityInput} inputProps={{min:1}} onChange={(event) => handleQuantityChange(event, setQuantity)}/>
   <Button color="primary" variant="contained">Add To Cart</Button>
   </CardActions>
   </Card> 
